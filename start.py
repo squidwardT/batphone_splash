22d3ff3719ee5651826162220382debfaea79432
squidwardT <lavigne.de@husky.neu.edu>
2015-12-02 23:03:07 -0500

    App starter working

--- start.py
+++ start.py
@@ -3,6 +3,16 @@ import threading
 import webbrowser
 from batman_splash import start_app
 
-thread = threading.Thread(target=webbrowser.open, args=('http://localhost:5000',))
-thread.start()
-start_app()
+def start_browser(server_ready_event, url):
+	server_ready_event.wait()
+	webbrowser.open(url)
+
+if __name__ == '__main__':
+	url = 'http://localhost:5000'
+	server_ready = threading.Event()
+	browser_thread = threading.Thread(target=start_browser,
+					  args = (server_ready, url))
+	browser_thread.start()
+	server_ready.set()
+	start_app()
+	browser_thread.join()
